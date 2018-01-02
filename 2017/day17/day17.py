7f5023d76e5f57b9e183d874921589c13035bf3b
Krzysztof Urbańczyk <urbanczykkr@gmail.com>
2018-01-02 22:20:39 +0100

    day 17 part 2 - I used deque, thx reddit

--- 2017/day17/day17.py
+++ 2017/day17/day17.py
@@ -1,43 +1,23 @@
+from collections import deque
+
 with open('input') as f:
     _input = f.readline()
 
 steps = int(_input)
-array = [0]
-index = 0
+array = deque([0])
 
 for cnt in range(1, 2018):
-    if len(array) > steps:
-        index += steps
-        if index >= len(array):
-            index = index - len(array)
-    else:
-        for _ in range(steps):
-            index += 1
-            if len(array) <= index:
-                index = 0
-
-    array.insert(index + 1, cnt)
-    index = index + 1
-
-print(array[index + 1])
-
-#part 2
-array = [0]
-index = 0
-
-for cnt in range(1, 5000001):
-    if len(array) > steps:
-        index += steps
-        if index >= len(array):
-            index = index - len(array)
-    else:
-        for _ in range(steps):
-            index += 1
-            if len(array) <= index:
-                index = 0
-
-    array.insert(index + 1, cnt)
-    index = index + 1
+    array.rotate(-steps)
+    array.append(cnt)
 
 print(array[0])
+
+#part 2
+array = deque([0])
+
+for cnt in range(1, 50000001):
+    array.rotate(-steps)
+    array.append(cnt)
+
+array.rotate(-array.index(0))
 print(array[1])
